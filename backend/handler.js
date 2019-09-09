ac253a522bea601c1e1610303c917c500eef3b5f
singhlovepreet9 <lovepreet.singh61182@gmail.com>
2019-09-09 18:07:52 +0530

    first commit

---
+++ backend/handler.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const moment = require("moment");
+
+module.exports.logger = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      timeStamp: moment().unix()
+    })
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
