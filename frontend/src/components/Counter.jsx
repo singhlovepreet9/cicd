1412a1ae22b7fe71d9b62333d92befaac61f0227
singhlovepreet9 <lovepreet.singh61182@gmail.com>
2019-09-10 23:28:54 +0530

    hello falcon

--- frontend/src/components/Counter.jsx
+++ frontend/src/components/Counter.jsx
@@ -30,7 +30,7 @@ class Counter extends Component {
   render() {
     return (
       <div style={{ align: "center" }}>
-        Serverless {this.state.message}
+        Serverless cicd {this.state.message}
         <div>
           <Button varaint="outlined" onClick={this.decrement} color="secondary">
             -
