50973686b9f7dd747d373cda9bdd6beae3c5f05e
singhlovepreet9 <lovepreet.singh61182@gmail.com>
2019-09-11 15:38:08 +0530

    branch

--- frontend/src/components/Counter.jsx
+++ frontend/src/components/Counter.jsx
@@ -30,7 +30,7 @@ class Counter extends Component {
   render() {
     return (
       <div style={{ align: "center" }}>
-        Serverless cicd {this.state.message}
+        Serverless PR worked {this.state.message}
         <div>
           <Button varaint="outlined" onClick={this.decrement} color="secondary">
             -
